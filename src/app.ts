05027db35fe2a52d66e9b15c02752cfa8f26eeed
DenysRogeres <dennysrogeres@gmail.com>
2021-10-24 13:44:22 -0300

    Terminando aplicação node.js

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
 import "dotenv/config";
 import express, { json } from 'express';
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
 import { router } from "./routes";
 
-const PORT = 4000;
-
 const app = express();
+app.use(cors())
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp, {
+    cors: {
+        origin: "*"
+    }
+});
+
+io.on("connection", socket => {
+    console.log(`Usuário conectado no socket ${socket.id}`);
+})
 
 app.use(express.json());
 app.use(router);
@@ -21,6 +36,4 @@ app.get('/signin/callback', (request, response) => {
     return response.json(code);
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+export { serverHttp, io}
